e6575ce10805b93788e325390033d52e6e723a66
Duy Linh <linhtdts02636@gmail.com>
2026-07-19 22:21:44 +0700

    fix: auth not working leading to failure of all user pages, now uses data-auth tag in body to auth, cleaned up style.css

--- frontend/utils/core.js
+++ frontend/utils/core.js
@@ -4,9 +4,11 @@ document.write('<script src="/frontend/utils/constants.js"><\/script>');
 document.write('<script src="/frontend/services/api.js"><\/script>');
 document.write('<script src="/frontend/services/authService.js"><\/script>');
 
-if (window.location.pathname.includes('/pages/user/')) {
+if (document.body.dataset.auth === 'user') {
     document.body.style.display = 'none';
-    checkAuth().then(function () {
-        document.body.style.display = '';
+    document.addEventListener('DOMContentLoaded', function () {
+        checkAuth().then(function () {
+            document.body.style.display = '';
+        });
     });
 }
